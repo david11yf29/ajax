24b6ccdadb0af436e62a37cee3bcc506bed4f8d6
david11yf29 <david11yf29@gmail.com>
2020-04-08 23:12:25 +0800

    updated

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import axios from '../../axios';
 
 import './FullPost.css';
 
@@ -14,7 +15,7 @@ class FullPost extends Component {
     componentDidUpdate() {
         if(this.props.id) {
             if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+                axios.get(`posts/${this.props.id}`)
                     .then(response => {
                         console.log(response);
                         this.setState({
