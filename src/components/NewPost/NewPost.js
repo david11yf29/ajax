f0e164408a6c7368a86d32353a2ba90f8235fec3
david11yf29 <david11yf29@gmail.com>
2020-04-08 21:33:13 +0800

    updated

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -11,7 +11,15 @@ class NewPost extends Component {
     }
 
     postDataHandler = () => {
-
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        };
+        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+            .then(response => {
+                console.log(response);
+            })
     }
 
     render () {
@@ -27,7 +35,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
