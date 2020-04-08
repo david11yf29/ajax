f195cbf11e4350dec00223a7add8d0b684389294
david11yf29 <david11yf29@gmail.com>
2020-04-08 20:04:09 +0800

    updated

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,21 +7,35 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
 
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
+                this.setState({
+                    posts: response.data
+                })
                 console.log(response);
             })
     }
 
     render () {
+
+        const posts = this.state.posts.map(
+            (post) => {
+                return <Post key={post.id} title={post.title} />
+            }
+        )
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
